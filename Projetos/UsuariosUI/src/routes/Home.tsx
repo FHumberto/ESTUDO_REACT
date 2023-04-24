7d366cac087f73a28224a8d78210e1f60650ae3a
Humberto Guedes <franciscohguedes@gmail.com>
2023-04-24 09:46:51 -0300

    [UsuariosUi] add: consumo de rota de listagem

--- Projetos/UsuariosUI/src/routes/Home.tsx
+++ Projetos/UsuariosUI/src/routes/Home.tsx
@@ -1,6 +1,56 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Usuario } from "../types/Usuario";
+import './Home.css'
+
 const Home = () =>
 {
-    return <div>Home</div>;
+    const [usuarios, setUsuarios] = useState<Usuario[]>([]);
+
+    const getUsuarios = async () =>
+    {
+        try
+        {
+            const response = await axios.get("https://localhost:7099/api/v1/usuario");
+            const data = response.data;
+            setUsuarios(data);
+        } catch (error)
+        {
+            console.log(error);
+        }
+    };
+
+    useEffect(() =>
+    {
+        getUsuarios();
+    }, []);
+
+    return (
+        <div>
+            {usuarios.length === 0 ? (
+                <p>Carregando...</p>
+            ) : (
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nome</th>
+                            <th>E-mail</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {usuarios.map((usuario) => (
+                            <tr key={usuario.id}>
+                                <td>{usuario.id}</td>
+                                <td>{usuario.nome}</td>
+                                <td>{usuario.email}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            )}
+        </div>
+    );
 };
 
 export default Home;
